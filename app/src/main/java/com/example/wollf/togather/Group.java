1ef6756591d103372b4a5caca9b52f2dfb60e99f
wollf92 <wollf92@hotmail.com>
2017-10-17 16:02:45 +0200

    settings

--- app/src/main/java/com/example/wollf/togather/Group.java
+++ app/src/main/java/com/example/wollf/togather/Group.java
@@ -1,9 +1,16 @@
 package com.example.wollf.togather;
 
+import java.util.List;
+
 /**
  * Created by wollf on 17-10-2017.
  */
 
 public class Group {
+    String groupName;
+    List<User> users;
+    public void addUser(User u){
+        users.add(u);
+    }
 
 }
