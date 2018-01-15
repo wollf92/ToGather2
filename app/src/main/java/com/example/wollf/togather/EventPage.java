02fad67a063bcbe2cae5d6191b248122c6d75c05
Mark Kuhar <kuharmark@gmail.com>
2018-01-15 22:51:08 +0100

    whatsapp in event page

--- app/src/main/java/com/example/wollf/togather/EventPage.java
+++ app/src/main/java/com/example/wollf/togather/EventPage.java
@@ -1,9 +1,15 @@
 package com.example.wollf.togather;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,7 +33,6 @@ public class EventPage extends AppCompatActivity {
         TextView desc = findViewById(R.id.event_desc_show);
         TextView starts = findViewById(R.id.event_from_show);
         TextView ends = findViewById(R.id.event_to_show);
-        TextView group = findViewById(R.id.event_group_show);
 
         String startDate = String.format("%1$td.%1$tm.%1$tY", e.getStartDate());
         String endDate = String.format("%1$td.%1$tm.%1$tY", e.getEndDate());
@@ -42,9 +47,38 @@ public class EventPage extends AppCompatActivity {
         desc.setText(e.getDescription());
         starts.setText(startDate + " " + start_time);
         ends.setText(endDate + " " + end_time);
-        group.setText(e.getGroup().getGroupName());
 
+        LayoutInflater inflater = (LayoutInflater) this
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LinearLayout members_view = findViewById(R.id.event_member_names);
+
+        String res = "";
+
+        for (final User u : e.getGroup().getUsers()){
+            View group_member = inflater.inflate(R.layout.group_member, null, false);
+            TextView member_name = group_member.findViewById(R.id.member_name);
+            ImageButton member_send = group_member.findViewById(R.id.member_send);
+
+            member_send.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    openWhatsApp(u.getPhone(), u.getName());
+                }
+            });
+            member_name.setText(u.getName());
+            members_view.addView(group_member);
+        }
     }
+
+    private void openWhatsApp(String number, String name) {
+        Intent sendIntent = new Intent(Intent.ACTION_SEND);
+        sendIntent.setType("text/plain");
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "Hello " + name);
+        sendIntent.putExtra("jid", number + "@s.whatsapp.net"); //phone number without "+" prefix
+        sendIntent.setPackage("com.whatsapp");
+        this.startActivity(sendIntent);
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
